fba90792ebdf932382653499be6c5898c96a4714
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-20 04:09:49 +0600

    login page added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,59 @@
+import { Link } from "react-router-dom";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login</h2>
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body p-12 pb-0 mb-6">
+                        <div className="text-center">
+                            <h1 className="text-3xl font-bold">Login your account</h1>
+                            <hr className='mt-5' />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <Link to="#" className="label-text-alt link link-hover">Forgot password?</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-4">
+                            <button className="btn  bg-primary-color hover:bg-secondary-color border-primary-color hover:border-secondary-color">Login</button>
+                        </div>
+                        <p className='text-pink-start text-center mt-4'>error</p>
+                    </form>
+                    <div className='px-12 pb-12'>
+                        <hr />
+                        <p className='text-center mt-2'>or Continue with</p>
+                        <div className='flex gap-3 justify-center'>
+                            <div className="form-control mt-4">
+                                <button className="btn text-primary-color hover:text-white bg-white hover:bg-primary-color border-primary-color hover:border-primary-color px-6">
+                                    <FaGoogle className='text-2xl' />
+                                </button>
+                            </div>
+                            <div className="form-control mt-4">
+                                <button className="btn text-primary-color hover:text-white bg-white hover:bg-primary-color border-primary-color hover:border-primary-color px-6">
+                                    <FaGithub className='text-2xl' />
+                                </button>
+                            </div>
+                        </div>
+                        <div>
+                            <p className="label-text-alt mt-4 text-center">Don’t have An account ?
+                                <span> <Link className="link link-hover text-primary-color font-semibold" to="/signup">SignUp</Link> </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
