31158b3f87e95ff82dbbfe099277fb78803cad86
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-20 05:31:53 +0600

    email password signup implemented

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,12 +1,55 @@
 import { Link } from "react-router-dom";
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const SignUp = () => {
+
+    const [error, setError] = useState('');
+    const { createUser, updateUser } = useContext(AuthContext);
+
+    const handleSignUp = event => {
+        event.preventDefault();
+        setError('');
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        const photo = form.photo.value;
+
+        console.log(name, email, password, photo)
+
+        if (password.length < 6) {
+            setError('Please add at least 6 characters in your password');
+            return;
+        }
+
+        createUser(email, password)
+            .then(result => {
+                const createdUser = result.user;
+                console.log(createdUser);
+                updateUser(result.user, name, photo)
+                    .then(() => {
+                        console.log('profile updated');
+                        form.reset();
+                    })
+                    .catch(error => {
+                        console.error(error.message);
+                        setError(error.message);
+                    })
+            })
+            .catch(error => {
+                console.error(error.message);
+                setError(error.message);
+            })
+
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col ">
                 <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
-                    <form className="card-body p-12 pb-0 mb-6 sm:w-[400px]">
+                    <form onSubmit={handleSignUp} className="card-body p-12 pb-0 mb-6 sm:w-[400px]">
                         <div className="text-center">
                             <h1 className="text-3xl font-bold">Sign Up</h1>
                             <hr className='mt-5' />
@@ -16,12 +59,6 @@ const SignUp = () => {
                                 <span className="label-text">Your Name</span>
                             </label>
                             <input type="text" name='name' placeholder="name" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Photo URL</span>
-                            </label>
-                            <input type="text" name='photo' placeholder="url" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
@@ -35,10 +72,16 @@ const SignUp = () => {
                             </label>
                             <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                         </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" name='photo' placeholder="url" className="input input-bordered" required />
+                        </div>
                         <div className="form-control mt-4">
                             <button className="btn bg-primary-color hover:bg-secondary-color border-primary-color hover:border-secondary-color">Sign Up</button>
                         </div>
-                        <p className='text-pink-start text-center mt-4'>error</p>
+                        <p className='text-pink-start text-center mt-4'>{error}</p>
                     </form>
                     <div className='px-12 pb-12'>
                         <hr />
