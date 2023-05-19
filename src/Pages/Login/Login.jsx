db1d772c18326cb0e9d5b2357d729f36de0a1d96
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-20 05:50:43 +0600

    all login method implemented in login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,72 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
+    const [error, setError] = useState('');
+
+    const { signIn, googleSignIn, githubSignIn } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    console.log('login page location', location);
+    const from = location.state?.from?.pathname || '/';
+
+
+    const handleLogin = event => {
+        setError('');
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        signIn(email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                form.reset();
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.error(error.message);
+                setError(error.message);
+            })
+    }
+
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(result => {
+                const createdUser = result.user;
+                console.log(createdUser);
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.error(error.message);
+                setError(error.message);
+            })
+    }
+
+    const handleGithubSignIn = () => {
+        githubSignIn()
+            .then(result => {
+                const createdUser = result.user;
+                console.log(createdUser);
+                navigate(from, { replace: true })
+            })
+            .catch(error => {
+                console.error(error.message);
+                setError(error.message);
+            })
+    }
+
+
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col">
                 <div className="card flex-shrink-0 w-full shadow-2xl bg-base-100">
-                    <form className="card-body p-12 pb-0 mb-6 sm:w-[400px]">
+                    <form onSubmit={handleLogin} className="card-body p-12 pb-0 mb-6 sm:w-[400px]">
                         <div className="text-center">
                             <h1 className="text-3xl font-bold">Login</h1>
                             <hr className='mt-5' />
@@ -29,19 +89,19 @@ const Login = () => {
                         <div className="form-control mt-4">
                             <button className="btn  bg-primary-color hover:bg-secondary-color border-primary-color hover:border-secondary-color">Login</button>
                         </div>
-                        <p className='text-pink-start text-center mt-4'>error</p>
+                        <p className='text-pink-start text-center mt-4'>{error}</p>
                     </form>
                     <div className='px-12 pb-12'>
                         <hr />
                         <p className='text-center mt-2'>or Continue with</p>
                         <div className='flex gap-3 justify-center'>
                             <div className="form-control mt-4">
-                                <button className="btn text-primary-color hover:text-white bg-white hover:bg-primary-color border-primary-color hover:border-primary-color px-6">
+                                <button onClick={handleGoogleSignIn} className="btn text-primary-color hover:text-white bg-white hover:bg-primary-color border-primary-color hover:border-primary-color px-6">
                                     <FaGoogle className='text-2xl' />
                                 </button>
                             </div>
                             <div className="form-control mt-4">
-                                <button className="btn text-primary-color hover:text-white bg-white hover:bg-primary-color border-primary-color hover:border-primary-color px-6">
+                                <button onClick={handleGithubSignIn} className="btn text-primary-color hover:text-white bg-white hover:bg-primary-color border-primary-color hover:border-primary-color px-6">
                                     <FaGithub className='text-2xl' />
                                 </button>
                             </div>
