cced950e0d7e0729dbdbe6db711365d46c365268
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-05-21 01:58:04 +0600

    delete data from db implemented

--- src/Pages/MyToys/ToysRow.jsx
+++ src/Pages/MyToys/ToysRow.jsx
@@ -1,6 +1,38 @@
+import Swal from 'sweetalert2';
 
 const ToysRow = ({ toy }) => {
-    const { toyPhoto, toyName, toyCategory, toyPrice, availableQuantity, toyRating } = toy;
+    
+    const { _id, toyPhoto, toyName, toyCategory, toyPrice, availableQuantity, toyRating } = toy;
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/toys/${id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'this toy has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <tr>
@@ -8,7 +40,7 @@ const ToysRow = ({ toy }) => {
                 <div className="flex items-center space-x-6">
                     <div className="avatar">
                         <div className="mask mask-squircle w-14 h-14">
-                            <img src={toyPhoto} alt="toy photo" />
+                            {toyPhoto && <img src={toyPhoto} alt="toy photo" />}
                         </div>
                     </div>
                     <div className="space-y-2">
@@ -23,7 +55,7 @@ const ToysRow = ({ toy }) => {
             <th>
                 <div className="flex flex-col gap-2 w-fit">
                     <button className="btn btn-ghost btn-xs bg-primary-color">Update</button>
-                    <button className="btn btn-ghost btn-xs bg-secondary-color">Delete</button>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-ghost btn-xs bg-secondary-color">Delete</button>
                 </div>
             </th>
         </tr>
